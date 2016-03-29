1e5c2df2514806bdeb402740ee9eb10b5180b1ca
Vlad <vladislav-loboda@mail.ru>
2016-03-29 15:12:41 +0200

    delete unused import

--- app/src/main/java/first/task/yalantis/yalantischallenge/MainActivity.java
+++ app/src/main/java/first/task/yalantis/yalantischallenge/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
